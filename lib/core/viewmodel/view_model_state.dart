7504cbd8b5f25ff09364e1034d7d91362f222252
kilroy80 <kilroy80@gmail.com>
2023-09-22 10:41:44 +0900

    feat: view model widget state

---
+++ lib/core/viewmodel/view_model_state.dart
@@ -0,0 +1,55 @@
+import 'package:clean_arch_core/core/viewmodel/view_model_mixin.dart';
+import 'package:flutter/material.dart';
+
+abstract class ViewModelState<T extends StatefulWidget, VM extends ViewModelMixin>
+    extends State<T>
+    with WidgetsBindingObserver {
+
+  late final VM _viewModel;
+  VM get viewModel => _viewModel;
+
+  @protected
+  bool get wantAppLifeCycle;
+
+  VM createViewModel();
+
+  void onAppResume();
+
+  void onAppPause();
+
+  void onWidgetReady();
+
+  @mustCallSuper
+  @override
+  void initState() {
+    super.initState();
+    _viewModel = createViewModel();
+
+    if (wantAppLifeCycle) {
+      WidgetsBinding.instance.addObserver(this);
+    }
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      onWidgetReady();
+    });
+  }
+
+  @mustCallSuper
+  @override
+  void dispose() {
+    if (wantAppLifeCycle) {
+      WidgetsBinding.instance.removeObserver(this);
+    }
+    _viewModel.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.resumed && wantAppLifeCycle) {
+      onAppResume();
+    } else if (state == AppLifecycleState.paused && wantAppLifeCycle) {
+      onAppPause();
+    }
+  }
+}
