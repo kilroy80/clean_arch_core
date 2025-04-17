a0859fefe60d402d82fc704b6034dd85e0b4daa2
kilroy80 <kilroy80@gmail.com>
2025-04-17 14:28:43 +0900

    feat: riverpod state firest create

---
+++ lib/core/riverpod/consumer_view_state.dart
@@ -0,0 +1,66 @@
+import 'package:clean_arch_core/core/viewmodel/view_model_mixin.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+abstract class ConsumerViewState<T extends ConsumerWidget, VM extends ViewModelMixin>
+    extends ConsumerState<T>
+    with WidgetsBindingObserver {
+
+  final String tag = T.toString();
+
+  late final VM _notifier;
+  VM get notifier => _notifier;
+
+  @protected
+  bool get wantAppLifeCycle;
+
+  @protected
+  Duration get postReadyMilliseconds => const Duration(milliseconds: 350);
+
+  VM createNotifier();
+
+  void onAppResume();
+
+  void onAppPause();
+
+  void onWidgetReady();
+
+  void onWidgetPostReady();
+
+  @mustCallSuper
+  @override
+  void initState() {
+    super.initState();
+    _notifier = createNotifier()..create();
+
+    if (wantAppLifeCycle) {
+      WidgetsBinding.instance.addObserver(this);
+    }
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      onWidgetReady();
+      Future.delayed(postReadyMilliseconds, () {
+        onWidgetPostReady();
+      });
+    });
+  }
+
+  @mustCallSuper
+  @override
+  void dispose() {
+    if (wantAppLifeCycle) {
+      WidgetsBinding.instance.removeObserver(this);
+    }
+    _notifier.dispose();
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (state == AppLifecycleState.resumed && wantAppLifeCycle) {
+      onAppResume();
+    } else if (state == AppLifecycleState.paused && wantAppLifeCycle) {
+      onAppPause();
+    }
+  }
+}
