18461f1116507324020e3c45af86c82a355318df
kilroy80 <kilroy80@gmail.com>
2025-04-20 21:04:42 +0900

    feat: Update code clean

--- example/lib/main_state.dart
+++ example/lib/main_state.dart
@@ -26,13 +26,53 @@ sealed class MainState extends MainBaseState
 //     _$MainStateFromJson(json);
 }
 
+// final stateProvider = StateProvider.autoDispose<int>((ref) {
+//   return 0;
+// });
+//
+// final provider = Provider.autoDispose<int>((ref) {
+//   return 0;
+// });
+//
+// final futureProvider = FutureProvider.autoDispose<int>((ref) {
+//   return 0;
+// });
+
+// final mainPageProvider = NotifierProvider.autoDispose<MainNotifier, MainState>(
+//   MainNotifier.new,
+// );
+
 final mainPageProvider = AutoDisposeNotifierProvider<MainNotifier, MainState>(
   MainNotifier.new,
 );
 
+// final mainPageProvider = AutoDisposeNotifierProviderFamily<
+//     MainFamilyNotifier, MainState, int>(MainFamilyNotifier.new,);
+
 class MainNotifier extends AutoDisposeNotifier<MainState> {
   @override
   MainState build() {
+    return const MainState.init();
+  }
+
+  Future<void> load() async {
+    state = const MainState.load(inc: 0);
+  }
+
+  Future<void> increase() async {
+    if (state is MainStateInit) return;
+
+    state = (state as MainStateLoad).copyWith(
+      inc: state.inc + 1,
+    );
+
+    // ref.notifyListeners();
+  }
+}
+
+class MainFamilyNotifier extends AutoDisposeFamilyNotifier<MainState, int> {
+  @override
+  MainState build(int arg) {
     return const MainState.init();
   }
 
