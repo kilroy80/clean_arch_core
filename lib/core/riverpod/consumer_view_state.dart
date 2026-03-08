90fe1aeca1b5349dc04ca13777459cffdac45c86
kilroy80 <kilroy80@gmail.com>
2026-03-08 23:33:05 +0900

    feat: Add ProviderConsumerState

--- lib/core/riverpod/consumer_view_state.dart
+++ lib/core/riverpod/consumer_view_state.dart
@@ -1,12 +1,89 @@
 import 'package:clean_arch_core/core/viewmodel/view_model_mixin.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/misc.dart';
 
 abstract class ConsumerViewState<T extends ConsumerStatefulWidget>
     extends ConsumerState<T>
     with WidgetsBindingObserver {
 
   final String tag = T.toString();
+
+  @protected
+  bool get wantAppLifeCycle;
+
+  @protected
+  Duration get postReadyMilliseconds => const Duration(milliseconds: 350);
+
+  void onAppResume();
+
+  void onAppPause();
+
+  void onWidgetReady();
+
+  void onWidgetPostReady();
+
+  @mustCallSuper
+  @override
+  void initState() {
+    super.initState();
+
+    if (wantAppLifeCycle) {
+      WidgetsBinding.instance.addObserver(this);
+    }
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      onWidgetReady();
+      Future.delayed(postReadyMilliseconds, () {
+        onWidgetPostReady();
+      });
+    });
+  }
+
+  @mustCallSuper
+  @override
+  void dispose() {
+    if (wantAppLifeCycle) {
+      WidgetsBinding.instance.removeObserver(this);
+    }
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    if (wantAppLifeCycle == false) return;
+
+    switch (state) {
+      case AppLifecycleState.resumed:
+        onAppResume();
+        break;
+      case AppLifecycleState.paused:
+        onAppPause();
+        break;
+      case AppLifecycleState.inactive:
+      // onAppInactive();
+        break;
+      case AppLifecycleState.detached:
+      // onAppDetached();
+        break;
+      case AppLifecycleState.hidden:
+      // onAppHidden();
+        break;
+    }
+  }
+}
+
+abstract class ProviderConsumerState<T extends ConsumerStatefulWidget, N, S>
+    extends ConsumerState<T>
+    with WidgetsBindingObserver {
+
+  final String tag = T.toString();
+
+  ProviderListenable<S> get provider;
+
+  S get state => ref.watch(provider);
+
+  N get notifier => ref.read((provider as dynamic).notifier) as N;
 
   @protected
   bool get wantAppLifeCycle;
